fece9b0033a24905b943d9c64be385d85747b0bb
Oguz Demirbasci <ogguzx@gmail.com>
2020-08-13 09:13:31 +0100

    slurm script modified

--- cluster_train_fconv.sh
+++ cluster_train_fconv.sh
@@ -7,6 +7,7 @@
 #SBATCH --mem=11400  # memory in Mb
 #SBATCH -t 96:00:00  # time requested in hour:minute:seconds
 #SBATCH --job-name=fconv_train
+#SBATCH --partition=CDT_Compute
 
 set -e # fail fast
 echo ${USER}
