0fc6a17ef912ea26ff73dbc9f1ce79e9beba4d42
Oguz Demirbasci <ogguzx@gmail.com>
2020-08-13 09:40:16 +0100

    slurm script modified

--- cluster_train_fconv.sh
+++ cluster_train_fconv.sh
@@ -2,7 +2,7 @@
 #SBATCH -o /home/%u/slurm_logs/slurm-%A.out
 #SBATCH -e /home/%u/slurm_logs/slurm-%A.out
 #SBATCH -N 1	  # nodes requested
-#SBATCH -n 8	  # tasks requested
+#SBATCH -n 4	  # tasks requested
 #SBATCH --gres=gpu:12  # use 4 GPU
 #SBATCH --mem=11400  # memory in Mb
 #SBATCH -t 8:00:00  # time requested in hour:minute:seconds
