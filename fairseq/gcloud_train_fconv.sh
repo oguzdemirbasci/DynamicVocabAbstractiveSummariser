d988d401175a240ab716a42fe0ce0570949d6245
Oguz Demirbasci <ogguzx@gmail.com>
2020-08-13 22:50:35 +0100

    scripts updated

--- fairseq/gcloud_train_fconv.sh
+++ fairseq/gcloud_train_fconv.sh
@@ -6,11 +6,11 @@ conda activate abs
 
 SAVEDIR='./checkpoints/fconv'
 
-# CUDA_LAUNCH_BLOCKING=1 python train.py ./data-bin --num-workers 12 --arch fconv_dvoc --lr 0.25 --clip-norm 0.1 --dropout 0.2 \
-#       --task dvoc_summarisation --skip-invalid-size-inputs-valid-test --max-target-positions 200 --max-source-positions 500 \
-#       --max-tokens 1000 --update-freq 4 --save-dir $SAVEDIR --keep-last-epochs 5 \
-#       --optimizer nag --criterion cross_entropy --truncate-source --truncate-target \
-#       --no-progress-bar --log-interval 100 2>&1 | tee $SAVEDIR/training.log
+CUDA_LAUNCH_BLOCKING=1 python train.py ./data-bin --num-workers 12 --arch fconv_dvoc --lr 0.25 --clip-norm 0.1 --dropout 0.2 \
+      --task dvoc_summarisation --skip-invalid-size-inputs-valid-test --max-target-positions 200 --max-source-positions 500 \
+      --max-tokens 1000 --update-freq 4 --save-dir $SAVEDIR --keep-last-epochs 5 --max-epoch 25\
+      --optimizer nag --criterion cross_entropy --truncate-source --truncate-target \
+      --no-progress-bar --log-interval 100 2>&1 | tee $SAVEDIR/training.log
 
 
 python /home/oguz/stop_instance.py
